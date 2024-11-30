17f9a8ca75cd5749d6f2671a034594b9547e38ab
starcrafter1 <starcrafter1345@gmail.com>
2024-11-30 20:18:04 +0200

    added update and read methods to article model

--- model/article.mjs
+++ model/article.mjs
@@ -6,9 +6,27 @@ const ARTICLE_PATH = path.join(process.cwd(), "data", "articles");
 class Article {
   async create(title, text) {
     const date = new Date();
-
     const id = Date.now();
-    
+
+    const data = {
+      id: id,
+      title: title,
+      text: text,
+      date: date,
+    };
+
+    const json = JSON.stringify(data);
+
+    await fs.writeFile(path.join(ARTICLE_PATH, String(id)), json);
+  }
+
+  async read(id) {
+    return JSON.parse(await fs.readFile(path.join(ARTICLE_PATH, String(id))));
+  }
+
+  async update(id, title, text) {
+    const date = new Date();
+
     const data = {
       id: id,
       title: title,
