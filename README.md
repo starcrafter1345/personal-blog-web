2b7cf9966079d88a092f3c97b8f200c2772d8921
starcrafter1 <starcrafter1345@gmail.com>
2024-12-02 20:17:10 +0200

    added readAll

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # personal-blog-web
-
-## TO-DO
-
-add to read return
-
-make readAll with cycle using somewhat fs.stats and read method
--- model/article.mjs
+++ model/article.mjs
@@ -1,5 +1,6 @@
 import * as path from "node:path";
 import * as fs from "node:fs/promises";
+import { arch } from "node:os";
 
 const ARTICLE_PATH = path.join(process.cwd(), "data", "articles");
 
@@ -21,7 +22,27 @@ class Article {
   }
 
   async read(id) {
-    return JSON.parse(await fs.readFile(path.join(ARTICLE_PATH, String(id))));
+    try {
+      return JSON.parse(await fs.readFile(path.join(ARTICLE_PATH, String(id))));
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async readAll() {
+    const objs = [];
+
+    try {
+      const articles = await fs.readdir(ARTICLE_PATH);
+
+      for (let article of articles) {
+        objs.push(await this.read(article));
+      }
+    } catch (error) {
+      throw error;
+    }
+
+    return objs;
   }
 
   async update(id, title, text) {
